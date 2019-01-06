c50eba27b5582912eaf5b623b563559df1bfc078
Kyle Davies <kdavies311@gmail.com>
2019-01-05 16:57:07 -0800

    linkedList completed. May need to come back and right more test

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,14 +2,38 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
+  list.previousAdd = null;
 
   list.addToTail = function(value) {
+    var node = Node(value);
+    if (list.tail !== null) {
+      list.tail.next = node;
+    }
+    if (list.head === null) {
+      list.head = node;
+    }
+    list.tail = node;
   };
 
   list.removeHead = function() {
+    var temp = list.head.value;
+    list.head = list.head.next;
+    return temp;
   };
 
   list.contains = function(target) {
+    var checkList = function(currentVal) {
+      if (currentVal.value === target) {
+        return true;
+      } else if (currentVal.next !== null) {
+        return checkList(currentVal.next);
+      }
+      return false;
+    };
+    return checkList(list.head);
+    // check if list heads value = target?
+    // if not, check if list head.next value = target
+    //if not check if next next...
   };
 
   return list;
