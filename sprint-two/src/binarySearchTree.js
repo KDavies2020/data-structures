754bab798c068ac5174bd6c932a3224c93f3dd47
Kyle Davies <kdavies311@gmail.com>
2019-01-08 11:55:33 -0800

    just binaryseachtree left - need to go back and update time complexity as well as write new tests.

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,27 @@
 var BinarySearchTree = function(value) {
+
 };
 
+BinarySearchTree.prototype.left = function() {
+// A .left property, a binary search tree (BST) where all values are lower than the current value.
+
+};
+
+BinarySearchTree.prototype.right = function() {
+//A .right property, a BST where all values are higher than the current value.
+};
+
+BinarySearchTree.prototype.insert = function(value) {
+// A .insert() method, which accepts a value and places it in the tree in the correct position.
+};
+
+BinarySearchTree.prototype.contains = function(value) {
+// A .contains() method, which accepts a value and returns a boolean reflecting whether or not the value is contained in the tree.
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(cb) {
+// A .depthFirstLog() method, which accepts a callback and executes it on every value contained in the tree.
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
